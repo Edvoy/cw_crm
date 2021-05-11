d84418c92e57f8fcb68ea590dfe2d984eec7baf1
Edvoy <edouard.voyer@gmail.com>
2021-05-11 20:36:10 +0200

    create company views

--- crm/main_crm/views.py
+++ crm/main_crm/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Company
+from .forms import AddCompanyForm
 
-# Create your views here.
+def index(request):
+    companies = Company.objects.all()
+    formatted_companies = ["<li>{}</li>".format(companies.company_name) for company in companies]
+    context = {
+        'company' : companies,
+    }
+    return render(request, 'companies/index.html', context)
+
+def addCompany(request):
+    form = AddCompanyForm(request.POST)
+    if form.is_valid():
+        form.save()
+    return redirect('/')
+
+def deleteCompany(request, id):
+    company = Company.objects.get(pk = id)
+    company.delete()
+    return redirect('/')
+
+def updateCompany(request, id):
+    company = Company.objects.get(pk = id)
+    updateForm = AddCompanyForm(instance = company)
+    if request.method == 'POST':
+        form = AddCompanyForm(request.POST, instance = company)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    context = {
+        'updateForm' : updateForm,
+        'key' : id,
+        'company' : Company.objects.all(),
+    }
+    return render(request, 'companies/index.html', context)
